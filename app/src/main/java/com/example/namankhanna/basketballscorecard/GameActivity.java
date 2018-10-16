497748ce5d0dd33c77b6c00aed1ef1549858625b
Naman Khanna <namankhanna97@gmail.com>
2018-10-16 17:03:32 +0530

    Third Commit

--- app/src/main/java/com/example/namankhanna/basketballscorecard/GameActivity.java
+++ app/src/main/java/com/example/namankhanna/basketballscorecard/GameActivity.java
@@ -1,13 +1,66 @@
 package com.example.namankhanna.basketballscorecard;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class GameActivity extends AppCompatActivity {
+
+    TextView tvTeam1, tvTeam2;
+    RecyclerView rvPlayers1, rvPlayers2;
+    TextView tvScore1, tvScore2;
+    String team1, team2;
+    ArrayList<Player> players1 = new ArrayList<>();
+    ArrayList<Player> players2 = new ArrayList<>();
+    PlayerAdapter adapter1, adapter2;
+    DatabaseHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        tvTeam1 = findViewById(R.id.tvName1);
+        tvTeam2 = findViewById(R.id.tvName2);
+        rvPlayers1 = findViewById(R.id.rvPlayers1);
+        rvPlayers2 = findViewById(R.id.rvPlayers2);
+        tvScore1 = findViewById(R.id.tvScore1);
+        tvScore2 = findViewById(R.id.tvScore2);
+        helper = new DatabaseHelper(this);
+        adapter1 = new PlayerAdapter(players1, this);
+        adapter2 = new PlayerAdapter(players2, this);
+
+        rvPlayers1.setLayoutManager(new LinearLayoutManager(this));
+        rvPlayers1.setAdapter(adapter1);
+
+        rvPlayers2.setLayoutManager(new LinearLayoutManager(this));
+        rvPlayers2.setAdapter(adapter2);
+
+        team1 = getIntent().getStringExtra("TEAM1");
+        team2 = getIntent().getStringExtra("TEAM2");
+        tvTeam1.setText(team1);
+        tvTeam2.setText(team2);
+
+        readPlayersFromDatabase();
+    }
+
+    private void readPlayersFromDatabase() {
+        Cursor cursor1 = helper.readPlayers(team1);
+        Cursor cursor2 = helper.readPlayers(team2);
+        while (cursor1.moveToNext()) {
+            String name = cursor1.getString(cursor1.getColumnIndex("Name"));
+            int num = cursor1.getInt(cursor1.getColumnIndex("TNo"));
+            players1.add(new Player(name, num));
+        }
+        while (cursor2.moveToNext()) {
+            String name = cursor2.getString(cursor2.getColumnIndex("Name"));
+            int num = cursor2.getInt(cursor2.getColumnIndex("TNo"));
+            players2.add(new Player(name, num));
+        }
     }
 }
